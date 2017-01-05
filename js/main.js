e3bdcfd6ab371909cddb1b1b62cd4bb0f0ece016
EpicIvo <ivoworks@gmail.com>
2017-01-05 06:57:12 +0100

    Added code for working first section, second section needs refinement

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
 window.addEventListener('load', init);
-
 function init() {
-    scroll();
+    window.addEventListener('scroll', onScroll, false);
+}
+let ticking = false;
+let latestKnownScrollY;
+
+function onScroll() {
+    latestKnownScrollY = window.scrollY;
+    requestTick();
+}
+function requestTick() {
+    if (!ticking) requestAnimationFrame(animate);
+    ticking = true;
+}
+function animate() {
+    ticking = false;
+    sectionOneAnimation();
 }
 
-function scroll() {
-    let scrollY = document.getElementById('body').scrollTop;
-    if (scrollY > 1){
-        document.getElementById('gameOnPhoneImage').style.marginTop = '-100%';
-    }else{
-        document.getElementById('gameOnPhoneImage').style.marginTop = '14%';
+//Section One
+function sectionOneAnimation() {
+    let currentScrollY = latestKnownScrollY;
+    const secondHill = document.getElementById('secondHillImage');
+    const thirdHill = document.getElementById('thirdHillImage');
+    secondHill.style.bottom = '-' + (currentScrollY * 0.05) + '%';
+    thirdHill.style.bottom = '-' + (currentScrollY * 0.16) + '%';
+    if ((currentScrollY * 0.05) > 80) {
+        sectionTwoAnimationStart()
     }
-    console.log('pos: ' + scrollY)
+}
+
+//Section Two
+function sectionTwoAnimationStart() {
+    let currentScrollY = latestKnownScrollY;
+    const mountain1 = document.getElementById('mountain1Image');
+    mountain1.style.bottom = (currentScrollY * 0.23) + '%';
+    if ((currentScrollY * 0.23) > 80) {
+        mountain1.style.bottom = '0%';
+        console.log('bottom: ' + mountain1.style.bottom);
+    } else if ((currentScrollY * 0.23) < 80) {
+        mountain1.style.bottom = (currentScrollY * 0.23) + '%';
+    }
 }
